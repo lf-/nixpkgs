5364b99fe538ece3639e6ad66f1d4dfa0918f726
Jade Lovelace <jadel@mercury.com>
2022-05-19 14:59:40 -0700

    hmm dont build ghc

--- pkgs/development/haskell-modules/generic-builder.nix
+++ pkgs/development/haskell-modules/generic-builder.nix
@@ -538,8 +538,7 @@ stdenv.mkDerivation ({
       remove-references-to -t $out $x
     done
     mkdir -p $doc
-    ''}
-    ${optionalString doIdeInfo ''
+    ''}${optionalString doIdeInfo ''
     find ${docdir "$doc"}/hie -name '*.hie' -exec remove-references-to -t $out '{}' ';'
     ''}
     ${optionalString enableSeparateDataOutput "mkdir -p $data"}
