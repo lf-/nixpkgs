f89e2e14b3b176e54a419c8eb3144c17deb1ac56
Jade Lovelace <jadel@mercury.com>
2022-05-19 15:04:27 -0700

    fourth times the charm

--- pkgs/development/haskell-modules/generic-builder.nix
+++ pkgs/development/haskell-modules/generic-builder.nix
@@ -538,7 +538,8 @@ stdenv.mkDerivation ({
       remove-references-to -t $out $x
     done
     mkdir -p $doc
-    ''}${optionalString doIdeInfo ''find ${docdir "$doc"}/hie -name '*.hie' -exec remove-references-to -t $out '{}' ';' ''}
+    ''}${optionalString doIdeInfo && doHaddock ''
+      find ${docdir "$doc"}/hie -name '*.hie' -exec remove-references-to -t $out '{}' ';' ''}
     ${optionalString enableSeparateDataOutput "mkdir -p $data"}
 
     runHook postInstall
